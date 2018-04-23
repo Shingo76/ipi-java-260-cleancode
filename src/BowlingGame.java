f0e48a3c507c0e651b9cb1abd754d6357b3a0961
Shingo76 <gaetan.inidjel@laposte.net>
2018-04-23 13:55:18 +0200

    Rendu TP cleanCode / tests Bowling

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -5,8 +5,61 @@ public class BowlingGame {
     public int score() {
         return score;
     }
-
-    public void roll(int numberPins) {
-        score += numberPins;
+    
+    int coupsParTour = 1;
+    //Si le calcul du taux de cotisation est sur salaire  (exemple commentaire pas à jour)
+    boolean spareBonus = false;
+    int strikeBonus = 0;
+    int scoreParTour = 0;
+    int spearCompteur = 0;
+    
+    //Exemple nom méthode non approprié et non prononçable (sauf si on parle japonais)
+    public void 美味しいパンティー(int numberPins) {
+    	
+    	//Si le premier coup n'est pas un strike
+    	if(numberPins != 10) {
+    	
+	    	if(coupsParTour == 1) {
+	    		scoreParTour = numberPins;
+	    		coupsParTour++;
+	    	}
+	    	else if (coupsParTour == 2) 
+	    		{
+	    		scoreParTour += numberPins;
+	    		coupsParTour--;
+		    		if(scoreParTour == 10 && numberPins != 0) {
+		    			spearCompteur = 2;
+		    		}
+		    		scoreParTour = 0;
+	    		}
+	    	} 
+    	else
+    	{
+    		strikeBonus = 4;
+    		coupsParTour++;
+    	}
+    	
+    	if(spearCompteur == 2) 
+    	{
+    		spearCompteur--;    	
+    	} 
+    	else if(spearCompteur == 1 ) 
+    	{
+    		numberPins = numberPins * 2;
+    		spearCompteur--;
+    	}
+    	
+    	if(strikeBonus == 4) {
+    		strikeBonus--;
+    		
+    	}
+    	else if(strikeBonus == 3 || strikeBonus == 2 || strikeBonus == 1) 
+    	{
+    		numberPins = numberPins * 2;
+    		strikeBonus--;
+    	}
+    	
+    	score += numberPins;
+        
     }
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -7,31 +7,69 @@ public class BowlingGameTest {
 
     BowlingGame game = new BowlingGame();
 
+    //Exemple nom méthode qui ne décrit pas son fonctionnement
     @Test
-    public void queDesGoutieresDonne0Points() {
-        rollMany(20, 0);
+    public void test1QFZP() {
+        美味しいパンティーMany(20, 0);
         assertEquals(0, game.score());
     }
 
+   
+    /**
+     * Méthode qui déploie un avion dans le ciel
+     * @param latAPPTUX
+     * 			La latitude qui défin
+     */
+    // Exemple javadoc pas à jour et paramètres non défininis et non utilisés
+    // Exemple de nom de méthode similaire à la précedente
     @Test
-    public void uneSeuleQuille(){
-        game.roll(1);
-        rollMany(19, 0);
+    public void test1OFZP(){
+        game.美味しいパンティー(1);
+        美味しいパンティーMany(19, 0);
         assertEquals(1, game.score());
     }
 
     @Test
     public void bonusDuSpare(){
-        game.roll(8);
-        game.roll(2);
-        game.roll(1);
-        rollMany(17, 0);
+        game.美味しいパンティー(8);
+        game.美味しいパンティー(2);
+        game.美味しいパンティー(1);
+        美味しいパンティーMany(17, 0);
         assertEquals(12, game.score());
     }
-
-    private void rollMany(int numberRolls, int numberPins) {
-        for (int i = 0; i < numberRolls; i++) {
-            game.roll(numberPins);
+    
+    @Test
+    public void bonusDuStrike(){
+        game.美味しいパンティー(10);
+        game.美味しいパンティー(0);
+        game.美味しいパンティー(2);
+        game.美味しいパンティー(1);
+        美味しいパンティーMany(17, 0);
+        assertEquals(16, game.score());
+    }
+    
+    @Test
+    public void testPartieStrikeEtSpears() {
+    	game.美味しいパンティー(10); //10 pts et strike
+    	game.美味しいパンティー(0);
+    	
+        game.美味しいパンティー(8); // 16 strikeBonus
+        game.美味しいパンティー(1); // 2 strikeBonus
+        
+        game.美味しいパンティー(2); // 2 pts
+        game.美味しいパンティー(8); // 8 pts et spear
+        
+        game.美味しいパンティー(5); // 10 pts spearBonus
+        game.美味しいパンティー(2); // 2 pts
+        
+        game.美味しいパンティー(8); // 8 pts
+        美味しいパンティーMany(12, 0);
+        assertEquals(10+16+2+2+8+10+2+8, game.score());
+    }
+    
+    private void 美味しいパンティーMany(int number美味しいパンティーs, int numberPins) {
+        for (int i = 0; i < number美味しいパンティーs; i++) {
+            game.美味しいパンティー(numberPins);
         }
     }
 }
